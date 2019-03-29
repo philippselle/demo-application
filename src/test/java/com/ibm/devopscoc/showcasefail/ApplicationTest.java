c4a4baa50dc5ba892493a80cd4a401910b0adce1
Philipp Selle <sellep@de.ibm.com>
2019-03-29 11:52:01 +0100

    add test

---
+++ src/test/java/com/ibm/devopscoc/showcasefail/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.ibm.devopscoc.showcasefail;
+
+import org.junit.Test;
+import com.ibm.devopscoc.showcasefail.Application;
+
+public class ApplicationTest {
+
+    @Test
+    public void main() {
+        Application.main(new String[] {});
+     }
+}
