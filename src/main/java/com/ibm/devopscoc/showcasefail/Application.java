83380df890f450297fe997bad38d94032a0c6b11
Philipp Selle <sellep@de.ibm.com>
2019-03-29 12:16:39 +0100

    force quality gate failure

--- src/main/java/com/ibm/devopscoc/showcasefail/Application.java
+++ src/main/java/com/ibm/devopscoc/showcasefail/Application.java
@@ -8,7 +8,18 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 public class Application {
 
+    public String publicString;
+    public int zero = 0;
 
+    // public String noReturn(String fail) {
+    //     boolean failure = true;
+    //     while (failure) {
+    //         //nothing
+    //         failure = false;
+    //     }
+    //     return "fail";
+    // }
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
